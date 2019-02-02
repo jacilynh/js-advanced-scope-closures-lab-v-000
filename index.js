b9140a3fb96107b336e3f7a5d0afd3e0fae2e599
Jacilyn Hayden <jacihayden@gmail.com>
2019-02-02 01:21:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,13 @@ function produceTipCalculator(percent) {
 // reference to a driverId that is incremented each time a new driver is created.
 // The rest of the code base does not have access to driverId.
 function createDriver() {
-  
+  let driverId = 0;
+  return class {
+    constructor(name) {
+      this.name = name;
+      this.id = ++driverId;
+    }
+  }
 }
 
 
