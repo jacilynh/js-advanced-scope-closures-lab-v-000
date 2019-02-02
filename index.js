ade30a255fe9d40545f6d88a333948503db8b826
Jacilyn Hayden <jacihayden@gmail.com>
2019-02-02 01:19:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-// Returns a function that then calculates whether a
-// given trip is within range. For example, `produceDrivingRange(10)` returns a
-// function that will return `false` if the trip is over 10 blocks distance and
-// `true` if the distance is within range. 
+// Returns a function that calculates whether a given trip is within range
 function produceDrivingRange(maxDistance) {
   return function (start, end) {
     let pathDistance = Math.abs(parseInt(start) - parseInt(end));
@@ -15,14 +12,13 @@ function produceDrivingRange(maxDistance) {
     }
   }
 }
-
-// Returns a function that then calculates a tip. For
-// example, `produceTipCalculator(.10)` returns a function that calculates ten
-// percent tip on a fare. `produceTipCalculator(.20)` returns a function that
-// calculates twenty percent tip on a fare.
-// function produceTipCalculator() {
+// Returns a function that calculates a tip. For example, `produceTipCalculator(.10)` returns a function that calculates ten percent tip on a fare. `produceTipCalculator(.20)` returns a function that calculates twenty percent tip on a fare.
+function produceTipCalculator(percent) {
+  return function(fare) {
+    return
+  }
   
-// }
+}
 
 // returns a Driver class. The class has
 // reference to a driverId that is incremented each time a new driver is created.
